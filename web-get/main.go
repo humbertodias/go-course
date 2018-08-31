28847f18ce957c795d62375db4cb44621b41fe16
Humberto Dias <humberto.dias@tangelogames.com>
2018-08-31 16:08:23 +0200

    GET Request

---
+++ web-get/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := &http.Client{
+		Timeout: time.Second * 30,
+	}
+	response, err := client.Get("https://www.google.es")
+	if err != nil {
+		fmt.Println("Erro ao abrir pagina do google. Error: ", err.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 {
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Erro na responsta: ", err.Error())
+			return
+		}
+		fmt.Println(string(body))
+	}
+
+	request, err := http.NewRequest("GET", "http://www.google.com", nil)
+	if err != nil {
+		fmt.Println("Erro na responsta: ", err.Error())
+		return
+	}
+	request.SetBasicAuth("test", "test")
+	response, err = client.Do(request)
+
+	if err != nil {
+		fmt.Println("Erro na responsta: ", err.Error())
+		return
+	}
+
+	if response.StatusCode == 200 {
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Erro na responsta: ", err.Error())
+			return
+		}
+		fmt.Println("SECOND REQUEST")
+		fmt.Println(string(body))
+	}
+
+}
